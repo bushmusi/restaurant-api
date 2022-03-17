b3a7f0f857df9f7e835f0dc930571269655b4979
bushmusi <bushera.mestofa@gmail.com>
2022-03-17 16:17:43 +0300

    Migration of tables

---
+++ database/migrations/2022_03_17_120808_create_stock_ins_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockInsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_ins', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('stock_item_id')->unsigned();
+            $table->integer('quantity')->unsigned();
+            $table->dateTime('indate');
+            $table->string('remark',255);
+
+            $table->foreign('stock_item_id')
+                  ->references('id')->on('stock_items')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_ins');
+    }
+}
---
+++ database/migrations/2022_03_17_120836_create_stock_wastages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockWastagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_wastages', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('stock_item_id')->unsigned();
+            
+            $table->integer('quantity')->unsigned();
+            $table->dateTime('wastage_date');
+            $table->boolean('isExpire');
+            $table->string('remark',100);
+
+            $table->foreign('stock_item_id')
+                  ->references('id')->on('stock_items')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_wastages');
+    }
+}
