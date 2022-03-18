b196b478c6eb7fcd176573c278d0ece5a4905647
bushmusi <bushera.mestofa@gmail.com>
2022-03-18 09:40:52 +0300

    Add json response method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function jsonReponse($success,$msg,$data,$status){
+        return response()->json([
+            'success' => $success,
+            'message' => $msg,
+            'data' => $data,
+        ],$status);
+    }
 }
