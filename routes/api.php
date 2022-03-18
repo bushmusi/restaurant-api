97413bc8e82bed02e8d36d323a6f2510392dab14
bushmusi <bushera.mestofa@gmail.com>
2022-03-18 17:13:16 +0300

    Add stock routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\SiUnitController;
 use App\Http\Controllers\Api\Stock\StockItemController;
+use App\Http\Controllers\Api\Stock\StockInController;
+use App\Http\Controllers\Api\Stock\StockOutController;
+use App\Http\Controllers\Api\EmployeeController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -35,5 +38,37 @@ Route::controller(StockItemController::class)->prefix('stock/')->group(function(
         Route::get('get/{id}','getStockItem');
         Route::get('filter-data','filterData');
         Route::delete('delete/{id}','deleteStockItem');
+    });
+});
+
+Route::controller(StockInController::class)->prefix('stock/')->group(function() {
+
+    Route::prefix('in/')->group(function () {
+        Route::post('create','create');
+        Route::put('update','update');
+        Route::get('get-all','getStockInAll');
+        Route::get('get/{id}','getStockInItem');
+        Route::get('get-stock-id/{stock_id}','getStockInByStckID');
+        Route::delete('delete/{id}','deleteStockIn');
+    });
+});
+
+Route::controller(EmployeeController::class)->prefix('employee/')->group(function() {
+    Route::post('create','create');
+    Route::put('update','update');
+    Route::get('get-all','getAll');
+    Route::get('get/{id}','getByID');
+    Route::delete('delete/{id}','delete');
+});
+
+Route::controller(StockOutController::class)->prefix('stock/')->group(function() {
+
+    Route::prefix('out/')->group(function () {
+        Route::post('create','create');
+        Route::put('update','update');
+        Route::get('get-all','getStockOutAll');
+        Route::get('get/{id}','getStockOutItem');
+        Route::get('get-stock-id/{stock_id}','getStockOutByStckID');
+        Route::delete('delete/{id}','deleteStockOut');
     });
 });
