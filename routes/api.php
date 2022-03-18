da9f48307b6c222ca2b83432880030f418a6e174
bushmusi <bushera.mestofa@gmail.com>
2022-03-18 15:21:34 +0300

    Update soft delete

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DepartmentController;
 use App\Http\Controllers\Api\SiUnitController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\Stock\StockItemController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -34,4 +24,16 @@ Route::controller(SiUnitController::class)->prefix('si-unit/')->group(function()
     Route::get('get-all','getAll');
     Route::get('get/{id}','getById');
     Route::delete('delete/{id}','deleteItem');
+});
+
+Route::controller(StockItemController::class)->prefix('stock/')->group(function() {
+
+    Route::prefix('item/')->group(function () {
+        Route::post('create','createStockItem');
+        Route::put('update','updateStockItem');
+        Route::get('get-all','getStockItemAll');
+        Route::get('get/{id}','getStockItem');
+        Route::get('filter-data','filterData');
+        Route::delete('delete/{id}','deleteStockItem');
+    });
 });
